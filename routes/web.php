b91526e019b558a33d6b6856275476056d615090
Aleksandr Zajac <mr.goodalex@gmail.com>
2019-05-28 02:08:26 +0300

    Копируем папку routes из 5.7 в 5.8

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,36 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//namespace добавляется папкой Blog
+Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function() {
+  //без 'namespace' => 'Blog', 'prefix' => 'blog' в
+  // Route::resource('posts', 'PostController') надо добавить
+  // Route::resource('Blog/posts', 'Blog/PostController')
+  Route::resource('posts', 'PostController')->names('blog.posts');
+});
+
+Route::resource('rest', 'RestTestController')->names('restTest');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Админка блога
+$groupData = [
+  'namespace' => 'Blog\Admin',
+  'prefix'    => 'admin/blog',
+];
+Route::group($groupData, function () {
+  //BlogCategory
+  $methods =['index', 'create', 'edit', 'store', 'update'];
+  Route::resource('categories', 'CategoryController')
+    ->only($methods)
+    ->names('blog.admin.categories');
+
+  // BlogPost
+    Route::resource('posts', 'PostController')
+      ->except(['show'])
+      ->names('blog.admin.posts');
+});
+
+//Route::resource('rest', 'RestTestController')->names('restTest');
