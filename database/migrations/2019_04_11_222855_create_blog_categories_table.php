d2a0511a7a1a87c616d04dc7ceb375f6f79c4df3
Aleksandr Zajac <mr.goodalex@gmail.com>
2019-05-30 23:37:04 +0300

    Исправление ошибок в модели User и замена increments('id') на bigIncrements('id')

--- database/migrations/2019_04_11_222855_create_blog_categories_table.php
+++ database/migrations/2019_04_11_222855_create_blog_categories_table.php
@@ -14,7 +14,7 @@ class CreateBlogCategoriesTable extends Migration
     public function up()
     {
         Schema::create('blog_categories', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->integer('parent_id')->unsigned()->default($value = 1);
             $table->string('slug')->unique();
             $table->string('title');
--- database/migrations/2019_04_11_225517_create_blog_posts_table.php
+++ database/migrations/2019_04_11_225517_create_blog_posts_table.php
@@ -14,7 +14,7 @@ class CreateBlogPostsTable extends Migration
     public function up()
     {
         Schema::create('blog_posts', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
 
             $table->integer('category_id')->unsigned();
             $table->integer('user_id')->unsigned();
